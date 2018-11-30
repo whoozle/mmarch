f6257aeb187a6ab085911a83b2bc2e5199ed5fbd
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-11-30 18:45:53 +0000

    added setup.py script

---
+++ python/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mmarch-pack",
+    version="1.0",
+    author="Vladimir Menshakov",
+    author_email="vladimir.menshakov@gmail.com",
+    description="Archiving utility for mmarch project, mmap-friendly archive",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/whoozle/mmarch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
